8560a5db6e86cecb085548675524f20ed0072417
GHXBR <ghxbrhd@gmail.com>
2023-10-29 15:58:32 +0300

    do vite and cap

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'job-skills',
+  webDir: 'dist',
+  server: {
+    url: '  http://localhost:5173'
+  }
+};
+
+export default config;
